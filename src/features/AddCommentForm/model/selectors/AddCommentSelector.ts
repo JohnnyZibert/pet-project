c8a725eb000c0ea4ef34916cb67afadb8f187501
zgirdan <zgirdan@mail.ru>
2023-04-13 09:17:33 +0300

    add commet

--- src/features/AddCommentForm/model/selectors/AddCommentSelector.ts
+++ src/features/AddCommentForm/model/selectors/AddCommentSelector.ts
@@ -1,4 +1,6 @@
 import { StateSchema } from 'app/providers/StoreProvider';
 
-export const getCommentTextSelector = (state: StateSchema) => state.addCommentForm?.text;
+export const getCommentTextSelector = (state: StateSchema) => state.addCommentForm?.text ?? '';
 export const getCommentErrorSelector = (state: StateSchema) => state.addCommentForm?.error;
+
+// git commet
